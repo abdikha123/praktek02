7e34852eae933b1b2223c16aeb62d641c6477516
abdi khanafy 16630929 <abdi.khanafy@gmail.com>
2018-05-06 22:08:21 +0700

    Membuat Atribut dan Behavior

--- src/praktek02/Square.java
+++ src/praktek02/Square.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class Square {
+    double sisi;
+
     
+   void cetakInfo(){
+        System.out.println("===============");
+        System.out.println("Sisi :+sisi");
+        System.out.println("Sisi :+sisi");
+        System.out.println("===============");
+    }
 }
